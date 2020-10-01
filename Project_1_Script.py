b340fe00a211a47c13d39b24a37533c0390b0cca
Patrick Geitner Jr <71834165+patgeitner@users.noreply.github.com>
2020-09-30 20:00:18 -0400

    Update Project_1_Script.py

--- Project_1_Script.py
+++ Project_1_Script.py
@@ -1,1 +1,13 @@
-print("Hello")
+import pandas as pd
+covid_df = pd.read_csv('covidCasesByCounty.csv')
+
+
+def covid_cases_by_state(state_abr):
+  cases_by_county = covid_df.iloc[:, 4:].set_index([covid_df['County Name'], covid_df["State"]])
+  total_cases_by_county = cases_by_county.sum(axis=1)
+  total_county_cases_by_state = total_cases_by_county[:,state_abr]
+  return total_county_cases_by_state
+  print(total_county_cases_by_state)
+  
+
+
