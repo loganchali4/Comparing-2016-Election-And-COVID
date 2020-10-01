d248291b85175a77e005816cd272afdecc4ce962
Patrick Geitner Jr <71834165+patgeitner@users.noreply.github.com>
2020-09-30 20:33:47 -0400

    Update Project_1_Script.py

--- Project_1_Script.py
+++ Project_1_Script.py
@@ -7,7 +7,11 @@ def covid_cases_by_state(state_abr):
   total_cases_by_county = cases_by_county.sum(axis=1)
   total_county_cases_by_state = total_cases_by_county[:,state_abr]
   return total_county_cases_by_state
-  print(total_county_cases_by_state)
-  
 
+def cases_by_state_over_time(state_abr):
+  cases_by_county = covid_df.iloc[:, 4:].set_index([covid_df['County Name'], covid_df["State"]])
+  total_cases_by_date = cases_by_county.sum()
+  return total_cases_by_date
 
+print(covid_cases_by_state('RI'))
+print(cases_by_state_over_time("AL"))
